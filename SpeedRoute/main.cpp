d4019dd4e5e8c3571999a3e54ddd2b0aeb275b82
Michel Kakulphimp <michelk@gmail.com>
2018-03-21 21:23:36 -0700

    Removing junk and adding Boost

--- SpeedRoute/main.cpp
+++ SpeedRoute/main.cpp
@@ -1,90 +1,70 @@
+// C++ Includes
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iterator>
 
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
+// Boost includes
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
-#include <SFML/Audio.hpp>
+// SFML Includes
 #include <SFML/Graphics.hpp>
 
-// Here is a small helper for you! Have a look.
-#include "ResourcePath.hpp"
+// Program Includes
+//#include "Types.h"
+//#include "Partitioner.h"
+//#include "NetList.h"
+//#include "Util.h"
+//#include "Types.h"
+//#include "Constants.h"
 
-int main(int, char const**)
+int main(int argc, char *argv[])
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
+    std::string filenameIn;
+    
+    // Declare the supported options
+    po::options_description desc("Usage");
+    po::variables_map vm;
+    po::positional_options_description p;
+    try
     {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
+        
+        desc.add_options()
+        ("help", "produce help message")
+        ("input-file", po::value<std::string>(&filenameIn)->default_value("..\\benchmarks\\C880.txt"),
+         "the input file");
+//        ("program-mode,p", po::value<unsigned int>(&programModeIn)->default_value(static_cast<unsigned int>(PROGRAM_MODE_GUI)),
+//         "set the program mode")
+//        ("draw-mode,d", po::value<unsigned int>(&drawModeIn)->default_value(static_cast<unsigned int>(DRAW_EVERY_NODE_PERCENT)),
+//         "set the draw mode")
+//        ("rounds,r", po::value<unsigned int>(&totalRounds)->default_value(NUMBER_OF_ROUNDS),
+//         "set the number of rounds");
+        
+        // This makes the input file a positional argument
+        p.add("input-file", -1);
+        po::store(po::command_line_parser(argc, argv).
+                  options(desc).positional(p).run(), vm);
+        po::notify(vm);
+    }
+    catch (const std::exception &exc)
+    {
+        // catch anything thrown within try block that derives from std::exception
+        std::cerr << exc.what();
+        return -1;
+    }
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    if (vm.count("input-file"))
+    {
+        std::cout << "Input file: "
+        << vm["input-file"].as<std::string>() << std::endl;
     }
 
     return EXIT_SUCCESS;
