9b9248ec2a353d6f726ed148fc393fd7a62b938f
Michel Kakulphimp <michelk@gmail.com>
2018-03-24 16:41:05 -0700

    Argument parser tweaks

--- SpeedRoute/main.cpp
+++ SpeedRoute/main.cpp
@@ -20,24 +20,29 @@ int main(int argc, char *argv[])
 {
     std::string netFilenameIn;
     std::string placementFilenameIn;
+    bool openClInfoFlag = false;
     
     // Declare the supported options
     po::options_description desc("Usage");
     po::variables_map vm;
     po::positional_options_description p;
+    
     try
     {
         desc.add_options()
         ("help", "produce help message")
-        ("netfile", po::value<std::string>(&netFilenameIn)->default_value("..\\benchmarks\\C880.txt"),
-         "the input net file")
-        ("placement", po::value<std::string>(&placementFilenameIn)->default_value("..\\benchmarks\\C880_placement.txt"),
-         "the input placement file");
+        ("netfile", po::value<std::string>(&netFilenameIn)->default_value("..\\benchmarks\\C880.txt"), "the input net file")
+        ("placementfile", po::value<std::string>(&placementFilenameIn)->default_value("..\\benchmarks\\C880_placement.txt"), "the input placement file")
+        ("openclinfo", po::bool_switch(&openClInfoFlag), "displays the OpenCL device support summart");
+        
+        po::store(po::command_line_parser(argc, argv).
+                  options(desc).positional(p).run(), vm);
+        po::notify(vm);
     }
     catch (const std::exception &exc)
     {
         // catch anything thrown within try block that derives from std::exception
-        std::cerr << exc.what();
+        std::cerr << exc.what() << std::endl;
         return -1;
     }
     if (vm.count("help"))
@@ -45,15 +50,20 @@ int main(int argc, char *argv[])
         std::cout << desc << std::endl;
         return 1;
     }
+    if (openClInfoFlag)
+    {
+        std::cout << "OpenCL info goes here" << std::endl;
+        return 1;
+    }
     if (vm.count("netfile"))
     {
         std::cout << "Net file: "
         << vm["netfile"].as<std::string>() << std::endl;
     }
-    if (vm.count("netfile"))
+    if (vm.count("placementfile"))
     {
         std::cout << "Placement file: "
-        << vm["placementFilenameIn"].as<std::string>() << std::endl;
+        << vm["placementfile"].as<std::string>() << std::endl;
     }
 
     return EXIT_SUCCESS;
